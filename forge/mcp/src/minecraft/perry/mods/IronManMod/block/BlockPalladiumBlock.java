6ae7ae4bb570e95a28048b991dec9e126da3d5e3
RealCloudCraft <RealCloudCraft@users.noreply.github.com>
2014-08-08 10:08:52 -0400

    Update BlockPalladiumBlock.java

--- forge/mcp/src/minecraft/perry/mods/IronManMod/block/BlockPalladiumBlock.java
+++ forge/mcp/src/minecraft/perry/mods/IronManMod/block/BlockPalladiumBlock.java
@@ -8,7 +8,7 @@ public class BlockPalladiumBlock extends Block{
 
 	public BlockPalladiumBlock(int par1, Material par2Material) {
 		super(par1, par2Material);
-		// TODO Auto-generated constructor stub
+		
 	}
 		public void registerIcons(IconRegister par1iconRegister) {
 			blockIcon = par1iconRegister.registerIcon("IronManMod:Palladium Block");
